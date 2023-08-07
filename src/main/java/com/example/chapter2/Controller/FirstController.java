96a9d23806091d0180e86b8eda75529776ccba41
Han <ehdrms001030@naver.com>
2023-08-07 17:58:39 +0900

    first commit

--- src/main/java/com/example/chapter2/Controller/FirstController.java
+++ src/main/java/com/example/chapter2/Controller/FirstController.java
@@ -1,2 +1,19 @@
-package com.example.chapter2.Controller;public class FirstController {
+package com.example.chapter2.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi") //페이지를 반환해달라는 URL요청을 접수
+    public String niceToMeetYou(Model model){ // model 객체 받아오기
+        model.addAttribute("username","한동근"); //model 객체가 value를 name에 연결해 브라우저 전송
+        return "greetings"; //서버가 알아서 templates 디렉토리에서 mustaches 파일을 찾아 브라우저로 전송
+    }
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname","한동근");
+        return "goodbye";
+    }
 }
--- src/main/java/com/example/chapter2/Controller/SelfCheckController.java
+++ src/main/java/com/example/chapter2/Controller/SelfCheckController.java
@@ -1,2 +1,18 @@
-package com.example.chapter2.Controller;public class SelfCheckController {
+package com.example.chapter2.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SelfCheckController {
+
+    @GetMapping("/random-quote")
+    public String randomQuote(Model model){
+        String[] quotes = {"행복은 습관이다. 그것을 몸에 지니라." + "-허버드","고개 숙이지 마십시오. 세상을 똑바로 정면으로 "
+                + "바라보십시오. -헬렌 켈러-","고난의 시기에 동요하지 않는 것, 이것은 진정" + "칭찬받을 만한 뛰어난 인물의 증거다 -베토벤-"};
+        int randInt = (int)(Math.random() * quotes.length);
+        model.addAttribute("randomquote",quotes[randInt]);
+        return "quote";
+    }
 }
