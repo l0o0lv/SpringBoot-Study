26051fb7bd0bd48d98468989b24a62e337d79f63
Han <ehdrms001030@naver.com>
2023-08-07 17:49:48 +0900

    first commit

---
+++ src/main/java/com/example/chapter2/Controller/SelfCheckController.java
@@ -0,0 +1,2 @@
+package com.example.chapter2.Controller;public class SelfCheckController {
+}
---
+++ src/test/java/com/example/chapter2/Chapter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chapter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
