7dfe7aaade32a00c4bace804ca3c192ea89fe4e7
Dong Geun <ehdrms001030@naver.com>
2023-09-24 15:36:49 +0900

    :white_check_mark: 4장 롬복 업데이트

--- src/main/java/com/example/chapter2/Controller/ArticleController.java
+++ src/main/java/com/example/chapter2/Controller/ArticleController.java
@@ -3,6 +3,8 @@ package com.example.chapter2.Controller;
 import com.example.chapter2.Dto.ArticleDto;
 import com.example.chapter2.Entity.ArticleEntity;
 import com.example.chapter2.Repository.ArticleRepositoryInterface;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ArticleController {
     @Autowired // 객체를 생성해주는 어노테이션으로 스프링부트가 미리 생성해 놓은 레포지토리 객체 주입.
     private ArticleRepositoryInterface articleRepositoryInterface;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @GetMapping("/articles/new")
     public String newArticleFrom(){
@@ -19,9 +22,11 @@ public class ArticleController {
     }
     @PostMapping("/articles/create")
     public String createArticle(ArticleDto articleDto){
-        System.out.println(articleDto.toString());
+        logger.info(articleDto.toString());
         ArticleEntity articleEntity = articleDto.toEntity();
+        logger.info(articleEntity.toString());
         ArticleEntity saved = articleRepositoryInterface.save(articleEntity);
+        logger.info(saved.toString());
         return "";
     }
 }
