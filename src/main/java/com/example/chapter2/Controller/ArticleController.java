b7b6111f76d0cab8088edff794863223171f66d2
Dong Geun <ehdrms001030@naver.com>
2023-09-26 02:26:31 +0900

    :white_check_mark: 5장 데이터 조회 업데이트

--- src/main/java/com/example/chapter2/Controller/ArticleController.java
+++ src/main/java/com/example/chapter2/Controller/ArticleController.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -28,5 +30,15 @@ public class ArticleController {
         ArticleEntity saved = articleRepositoryInterface.save(articleEntity);
         logger.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/articles/{id}") //데이터 조회
+    public String ReadArticle(@PathVariable Long id, Model model) //매개변수로 id를 받아옴
+    {
+        logger.info("id = " + id);
+        ArticleEntity articleEntity = articleRepositoryInterface.findById(id).orElse(null); //crudrepository가 제공하는 메소드
+        // id값으로 데이터를 찾을 때 해당 id가 없으면 null을 반환한다.
+        model.addAttribute("article",articleEntity); //article이라는 이름으로 articleenity객체 등록
+        return "articles/show";
     }
 }
